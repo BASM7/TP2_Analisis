79c38b943d82a0808d83dcf489ca55eb9455fc32
BASM7 <j.alonsomm07@gmail.com>
2020-11-30 21:16:36 -0600

    faltan deletes

--- GraphB.h
+++ GraphB.h
@@ -5,14 +5,23 @@
 #include <string>
 #include <sstream>
 
+class Edge;
+
 class Vertex {
-public:
     char value;
+public:    
     Vertex* nextVertex;
+    Edge* nextAdj;
 
     Vertex(char value, Vertex* nextVertex) {
         this->value = value;
         this->nextVertex = nextVertex;
+        this->nextAdj = nullptr;
+    }
+
+    char getValue() { return this->value; };
+    void setValue(char newValue) {
+        this->value = newValue;
     }
 
     ~Vertex() {};
@@ -20,9 +29,9 @@ public:
 
 class Edge {
 public:
-    Vertex* to;
-    double weight;
-    Edge* nextEdge;
+    Vertex* to = nullptr;
+    double weight = 0.0;
+    Edge* nextEdge = nullptr;
 
     Edge(Vertex* to, double weight, Edge* nextEdge) {
         this->to = to;
@@ -34,27 +43,23 @@ public:
 };
 
 class Graph{
-    int cantVertex;
-    int cantEdges;
-    Vertex* head;
+    int cantVertex = 0;
+    int cantEdges = 0;
+    Vertex* head = nullptr;
 
 public:    
 
-    Graph() {
-        head = nullptr;
-        cantVertex = 0;
-        cantEdges = 0;
-    }
+    Graph() {}
 
     bool isEmpty() { return cantVertex == 0; };
     Vertex* addVert(char newLabel);
     void deleteVert(Vertex* vertexToDelete);
     void changeLabel(Vertex* vertexToChange, char newLabel);
-    void createEdge(Vertex* vertex1, Vertex* vertex2);
+    void createEdge(Vertex* vertex1, Vertex* vertex2, double newWeight);
     void deleteEdge(Vertex* vertex1, Vertex* vertex2);
     void changeWeight(Vertex* vertex1, Vertex* vertex2, double newWeight);
     double getWeight(Vertex* vertex1, Vertex* vertex2);
-    char getLabel(Vertex* vertex) { return vertex->value; };
+    char getLabel(Vertex* vertex) { return vertex->getValue(); };
     Vertex* getHead() { return this->head; };
     Vertex* getNextVert(Vertex* vertex);
     Vertex* getFirstAdj(Vertex* vertex);
@@ -68,7 +73,11 @@ public:
 };
 
 Vertex* Graph::addVert(char newLabel) {
+    Vertex* newVertex = new Vertex(newLabel, this->head);
+    this->head = newVertex;
+    this->cantVertex++;
 
+    return newVertex;
 }
 
 void Graph::deleteVert(Vertex* vertexToDelete) {
@@ -77,11 +86,18 @@ void Graph::deleteVert(Vertex* vertexToDelete) {
 
 
 void Graph::changeLabel(Vertex* vertexToChange, char newLabel) {
-
+    if (vertexToChange)
+        vertexToChange->setValue(newLabel);
 }
 
-void Graph::createEdge(Vertex* vertex1, Vertex* vertex2) {
-
+void Graph::createEdge(Vertex* vertex1, Vertex* vertex2, double newWeight) {
+    if (vertex1 && vertex2) {
+        Edge* newEdge1 = new Edge(vertex1, newWeight, vertex2->nextAdj);
+        vertex2->nextAdj = newEdge1;
+        Edge* newEdge2 = new Edge(vertex2, newWeight, vertex1->nextAdj);
+        vertex1->nextAdj = newEdge2;
+        this->cantEdges++;
+    }
 }
 
 void Graph::deleteEdge(Vertex* vertex1, Vertex* vertex2) {
@@ -93,27 +109,69 @@ void Graph::changeWeight(Vertex* vertex1, Vertex* vertex2, double newWeight) {
 }
 
 double Graph::getWeight(Vertex* vertex1, Vertex* vertex2) {
-
+    double weight = 0.0;
+    bool done = false;
+    if (vertex1 && vertex2) {
+        Edge* tempEdge = vertex1->nextAdj;
+        while (tempEdge != nullptr && !done) {
+            if (tempEdge->to == vertex2) {
+                weight = tempEdge->weight;
+                done = true;
+            }
+            tempEdge = tempEdge->nextEdge;
+        }
+    }
+    return weight;
 }
 
 Vertex* Graph::getNextVert(Vertex* vertex) {
-
+    if (vertex != nullptr)
+        return vertex->nextVertex;
+    return nullptr;
 }
 
 Vertex* Graph::getFirstAdj(Vertex* vertex) {
-
+    if (vertex)
+        if (vertex->nextAdj)
+            return vertex->nextAdj->to;
+    return nullptr;
 }
 
-Vertex* Graph::getNextAdj(Vertex* vertex, Vertex* vertex2) {
-
+Vertex* Graph::getNextAdj(Vertex* vertex1, Vertex* vertex2) {
+    bool found = false;
+    if (vertex1 && vertex2) {
+        Edge* tempEdge = vertex1->nextAdj;
+        while (tempEdge != nullptr && !found) {
+            if (tempEdge->to == vertex2) {
+                found = true;
+                if (tempEdge->nextEdge) {
+                    return tempEdge->nextEdge->to;
+                }
+            }
+            tempEdge = tempEdge->nextEdge;
+        }
+    }
+    return nullptr;
 }
 
 bool Graph::isEdge(Vertex* vertex1, Vertex* vertex2) {
-
+    bool found = false;
+    if (vertex1 && vertex2) {
+        Edge* tempEdge = vertex1->nextAdj;
+        while (tempEdge != nullptr && !found) {
+            if (tempEdge->to == vertex2) {
+                found = true;
+            }
+            tempEdge = tempEdge->nextEdge;
+        }
+    }
+    return found;
 }
 
 int Graph::getNumAdjVertices(Vertex* vertex) {
+    int cant = 0;
 
+    return cant;
 }
 
 
--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <map>
 #include <unordered_set>
 #include <algorithm>
 #include <stack>
@@ -13,6 +12,7 @@ void printGraphWidth(Graph* graph) {
         Vertex* currentVertex = graph->getHead();
         std::queue<Vertex*> queue;
         std::unordered_set<Vertex*> visitedVertices;
+        std::unordered_set<Vertex*> alreadyPushedVertices;
 
         while (currentVertex != nullptr) {
             if (visitedVertices.find(currentVertex) == visitedVertices.end()) {
@@ -27,16 +27,18 @@ void printGraphWidth(Graph* graph) {
 
                     while (adjVertex != nullptr) {
                         if (visitedVertices.find(adjVertex) == visitedVertices.end()) {
-                            queue.push(adjVertex);
-                            visitedVertices.insert(adjVertex);
+                            if (alreadyPushedVertices.find(adjVertex) == alreadyPushedVertices.end()) {
+                                queue.push(adjVertex);
+                                alreadyPushedVertices.insert(adjVertex);
+                            }
+                            std::cout << graph->getLabel(tempVertex) << " -> ";
+                            std::cout << graph->getWeight(tempVertex, adjVertex);
+                            std::cout << " -> " << graph->getLabel(adjVertex) << std::endl;
                         }
-
-                        std::cout << graph->getLabel(tempVertex) << " -> ";
-                        std::cout << graph->getWeight(tempVertex, adjVertex);
-                        std::cout << " -> " << graph->getLabel(adjVertex) << std::endl;
 
                         adjVertex = graph->getNextAdj(tempVertex, adjVertex);
                     }
+                    visitedVertices.insert(tempVertex);
                 }
             }
             currentVertex = graph->getNextVert(currentVertex);
@@ -44,10 +46,80 @@ void printGraphWidth(Graph* graph) {
     }
 }
 
+// Algoritmo K.
+
+
+// Algoritmo L.
+double totalWeight(Graph* graph) {
+    double total = 0.0;
+    if (!graph->isEmpty()) {
+        Vertex* currentVertex = graph->getHead();
+        std::queue<Vertex*> queue;
+        std::unordered_set<Vertex*> visitedVertices;
+        std::unordered_set<Vertex*> alreadyPushedVertices;
+
+        while (currentVertex != nullptr) {
+            if (visitedVertices.find(currentVertex) == visitedVertices.end()) {
+                visitedVertices.insert(currentVertex);
+                queue.push(currentVertex);
+
+                while (!queue.empty()) {
+                    Vertex* tempVertex = queue.front();
+                    queue.pop();
+
+                    Vertex* adjVertex = graph->getFirstAdj(tempVertex);
+
+                    while (adjVertex != nullptr) {
+                        if (visitedVertices.find(adjVertex) == visitedVertices.end()) {
+                            if (alreadyPushedVertices.find(adjVertex) == alreadyPushedVertices.end()) {
+                                queue.push(adjVertex);
+                                alreadyPushedVertices.insert(adjVertex);
+                            }
+                            total += graph->getWeight(tempVertex, adjVertex);
+                        }
+                        adjVertex = graph->getNextAdj(tempVertex, adjVertex);
+
+                    }
+                    visitedVertices.insert(tempVertex);
+                }
+            }
+            currentVertex = graph->getNextVert(currentVertex);
+        }
+    }
+    return total;
+}
+
+// Algoritmo M.
+Vertex* getVertex(Graph* graph, char labelToSearch) {
+    Vertex* foundVertex = graph->getHead();
+    bool found = false;
+    while (foundVertex != nullptr && !found) {
+        if (graph->getLabel(foundVertex) == labelToSearch) {
+            return foundVertex;
+        }
+        foundVertex = graph->getNextVert(foundVertex);
+    }
+    return nullptr;
+}
+
+
 int main()
 {
     Graph* test_graph = new Graph();
+    Vertex* vA = test_graph->addVert('A');
+    Vertex* vB = test_graph->addVert('B');
+    Vertex* vC = test_graph->addVert('C');
+    Vertex* vD = test_graph->addVert('D');
 
+    test_graph->createEdge(vA, vB, 5.0);
+    test_graph->createEdge(vB, vC, 4.0);
+    test_graph->createEdge(vC, vA, 9.0);
+
+    printGraphWidth(test_graph);
+    
+    std::cout << totalWeight(test_graph) << std::endl;
+    std::cout << getVertex(test_graph, 'B')->getValue() << std::endl;
+    std::cout << getVertex(test_graph, 'B')->nextAdj->to->getValue() << std::endl;
 
     return 0;
 }
